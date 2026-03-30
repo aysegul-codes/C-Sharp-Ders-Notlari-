735048c5814e53187d41ff58cd8034245afca065
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-03-30 17:39:59 +0300

    Donguler_Toplam_Hesalama

---
+++ projeler3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace projeler3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {      //ÖRN : 1 ile 10 arasındaki sayıların (bu sayılar dahil) toplamını bulup sonucu ekrana yazan konsol uygulamasının C# dilinde kodunu yazınız.
+
+
+            try
+            {
+                int Sonuc = 0;
+                for (int i = 1; i <= 10; i++)
+                {
+                    Sonuc += i;
+                }
+                Console.WriteLine("1 ile 10 arasındaki sayıların toplamı {0}", Sonuc);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
