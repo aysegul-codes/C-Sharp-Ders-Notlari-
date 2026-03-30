5f51146c3ac3a49ea2f9f63e1b623a31bc685615
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-03-30 17:06:02 +0300

    Okul projeleri tertemiz şekilde eklendi

---
+++ projeler1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace projeler1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int SAYI1;
+            int SAYI2;
+            string METİN1;
+            string METİN2;
+            int SONUC;
+
+            Console.Write("1. Sayıyı Giriniz:");
+            METİN1 = Console.ReadLine();
+            SAYI1 = Convert.ToInt32(METİN1);
+
+            Console.Write("2. Sayıyı Giriniz:");
+            METİN2 = Console.ReadLine();
+            SAYI2 = Convert.ToInt32(METİN2);
+
+            SONUC = SAYI1 + SAYI2;
+            Console.WriteLine(SONUC.ToString());
+
+
+        }
+    }
+}
---
+++ projeler2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace projeler2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double SAYI1;
+            double SAYI2;
+            string METIN1;
+            string METIN2;
+            double SONUC;
+
+            Console.Write("1. Sayıyı Giriniz:");
+            METIN1 = Console.ReadLine();
+            SAYI1 = Convert.ToDouble(METIN1);
+
+            Console.Write("2. Sayıyı Giriniz:");
+            METIN2 = Console.ReadLine();
+            SAYI2 = Convert.ToDouble(METIN2);
+
+            SONUC = (SAYI1 + SAYI2)/2d;
+            Console.WriteLine("{0} sayısı ıle {1} Sayısının aritmetik ortalaması {2} olur",SAYI1,SAYI2,SONUC);
+        }
+    }
+}
