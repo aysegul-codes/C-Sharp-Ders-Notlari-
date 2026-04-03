cccc37f002a2e668148a1edea34ccfcf1c9ea239
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-04-03 14:42:04 +0300

    3 veya 5'e bölünen sayıları listeleme algoritması

---
+++ proje8/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace proje8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           // ÖRN: 1 ile 10 arasındaki sayılardan (bu sayılar dahil) 3’e veya 5’e tam bölünen sayıları bularak araya boşluk koyup ekrana yazan konsol uygulamasının C# dilinde kodunu yazınız.
+            try
+            {
+                string Sonuc = "";
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (i % 3 == 0 || i % 5 == 0)
+                    {
+                        Sonuc += i.ToString() + " ";
+                    }
+                }
+
+                Console.WriteLine("1 ile 10 arasındaki 3'e veya 5'e tam bölünen sayılar : {0}", Sonuc.Trim());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
+
