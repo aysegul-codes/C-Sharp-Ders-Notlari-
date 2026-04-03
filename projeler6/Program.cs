b74cd1d8a4a451329ad2e43512e807d97b20db80
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-04-03 14:27:00 +0300

    2 ve 3'e tam bölünebilen sayıların toplamını hesaplayan mantık eklendi.

---
+++ projeler6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace projeler6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ÖRN: 1 ile 10 arasındaki sayılardan(bu sayılar dahil) 2’ye ve 3’e tam olarak bölünebilen sayıların toplamını bularak sonucu ekrana yazan konsol uygulamasının C# dilinde kodunu yazınız.
+
+            try
+            {
+                int Sonuc = 0;
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (i % 2 == 0 && i % 3 == 0)
+                    {
+                        Sonuc += i;
+                    }
+                }
+                Console.WriteLine("1 ile 10 arasındaki hem 2'ye hemde 3'e bölünen sayıların toplamı {0}'dır.", Sonuc);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA: {0}", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
