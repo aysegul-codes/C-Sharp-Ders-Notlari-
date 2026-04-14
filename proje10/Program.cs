5def11636e4ccf9cfd6dd7a72c33b105e5fddb56
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-04-14 13:45:46 +0300

    3 elemanlı dizide en büyük sayıyı bulan program eklendi

---
+++ proje10/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace proje10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // ÖRN: Kullanıcı tarafından tam sayı olarak değerleri girilen 3 elemanlı bir dizinin en büyük elemanını bularak ekrana yazan konsol uygulamasının C# dilinde kodunu yazınız.
+            try
+            {
+                int[] SayiDizisi = new int[3];
+
+                for (int i = 0; i < SayiDizisi.Length; i++)
+                {
+                    Console.Write("Dizinin {0}. Elemanını Giriniz : ", i);
+                    string Metin = Console.ReadLine();
+                    SayiDizisi[i] = Convert.ToInt32(Metin);
+                }
+
+                int EnBuyuk = SayiDizisi[0];
+                string SayiMetin = "";
+
+                foreach (int Sayi in SayiDizisi)
+                {
+                    if (Sayi > EnBuyuk)
+                    {
+                        EnBuyuk = Sayi;
+                    }
+
+                    SayiMetin += Sayi.ToString() + "-";
+                }
+
+                Console.WriteLine("Girilen {0} sayıları arasındaki en büyük sayı {1} sayısıdır.",
+                    SayiMetin.Trim('-'), EnBuyuk);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
