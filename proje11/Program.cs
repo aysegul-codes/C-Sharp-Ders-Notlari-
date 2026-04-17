f72cfa58e436b5f4e4c1764f637d0cfb74f8500f
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-04-17 13:24:15 +0300

    Kelime baş harflerinden kısaltma üretildi

---
+++ proje11/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace proje11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ÖRN:Kullanıcı tarafından girilen bir metnin her bir kelmisenin baş harfını alarak araya nokta koyup kısaltmasını ekrana yazdıran konsol uygulamasının C# dilinde kodu yazınız.
+
+            try
+            {
+                Console.Write("Metni Giriniz : ");
+                string Metin = Console.ReadLine();
+
+                string[] kelimeDizisi = Metin.Split(' ');
+
+                string Kisaltma = "";
+
+                foreach (string kelime in kelimeDizisi)
+                {
+                    Kisaltma += kelime[0].ToString() + ".";
+                }
+
+                Console.WriteLine(Kisaltma.ToUpper());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
