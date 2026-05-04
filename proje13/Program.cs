95b1aa16b4feca7c63218c9e775b54ea8838822d
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-05-04 13:38:35 +0300

    metni ekrana tersten yazdıran konsol uygulaması

---
+++ proje13/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace proje13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       //ÖRN: Kullanıcı tarafından girilen bir metni ekrana tersten yazdıran konsol uygulamasının C# dilinde kodunu yazınız.
+            
+
+            try
+            {
+                Console.Write("Metni Giriniz: ");
+                string Metin = Console.ReadLine();
+
+                string TersMetin = "";
+                for (int i = Metin.Length-1; i >=0; i--)
+                {
+                    TersMetin += Metin[i].ToString();
+                }
+
+                Console.WriteLine("Girilen \"{0}\" metnin tersten yazılışı \"{1}\".",Metin, TersMetin );     // texsin içinde  \"{0}\" kullanmamızın sebebi "" içinde yazılmasını istediğimiz için.
+
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+            
+            finally
+            {
+                Console.ReadKey();
+            }
+
+
+        }   
+    }
+}
