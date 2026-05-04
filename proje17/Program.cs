5c801e31f5b7a7b5d37169cc435c497b0f8ebc9d
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-05-04 15:05:44 +0300

    Hazır fonksiyon kullanmadan tamsayı dizisi sıralama eklendi

---
+++ proje17/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace peoje17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ÖRN: Kullanıcı tarafından tam sayı olarak değerleri girilen 5 elemanlı bir dizinin hazır fonksiyon kullanmadan (sort, vb.) elemanlarını
+            //küçükten büyüğe sıralı olarak araya tire sembolü ekleyerek ekrana yazan konsol uygulamasının C# dilinde kodunu yazınız
+
+            try
+            {
+                int[] SayiDizisi = new int[5];
+
+                for (int i = 0; i < SayiDizisi.Length; i++)
+                {
+                    Console.Write("Dizinin {0}. Elemanını Giriniz : ", i);
+                    string Metin = Console.ReadLine();
+                    SayiDizisi[i] = Convert.ToInt32(Metin);
+                }
+
+                for (int i = 0; i < SayiDizisi.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < SayiDizisi.Length; j++)
+                    {
+                        if (SayiDizisi[i] > SayiDizisi[j])
+                        {
+                            int TEMP = SayiDizisi[i];
+                            SayiDizisi[i] = SayiDizisi[j];
+                            SayiDizisi[j] = TEMP;
+                        }
+                    }
+                }
+
+                string SayiMetin = "";
+                foreach (int Sayi in SayiDizisi)
+                {
+                    SayiMetin += Sayi.ToString() + "-";
+                }
+
+                Console.WriteLine("Girilen sayıların küçükten büyüğe sıralanmış şekli {0} 'dır.",
+                    SayiMetin.Trim('-'));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
