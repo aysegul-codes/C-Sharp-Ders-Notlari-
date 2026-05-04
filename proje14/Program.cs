a5f471d63d2bd6617c14271cd4d9a278888afad3
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-05-04 13:57:46 +0300

    Metnin palindrom olup olmadığını bulan konsol uygulaması

---
+++ proje14/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace proje14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // ÖRN:kullanıcı tarafından girilen bir metnin palindrom olup olmadığını bularak ekrana
+            // “Palindrom” ya da “Palindrom değil” yazan konsol uygulamasının C# dilinde kodunu yazınız.
+            try
+            {
+                Console.Write("Metni Giriniz: ");
+                string Metin = Console.ReadLine();
+
+                string TersMetin = "";
+                for (int i = Metin.Length - 1; i >= 0; i--)
+                {
+                    TersMetin += Metin[i].ToString();
+                }
+
+                if (Metin == TersMetin)
+                {
+                Console.WriteLine("Girilen \"{0}\" metnin tersten yazılışı \"{1}\" olduğu için girilen metin polindrom'dur.", Metin, TersMetin);
+
+                }
+ 
+                else
+                {
+                    Console.WriteLine("Girilen \"{0}\" metnin tersten yazılışı \"{1}\" olduğu için girilen metin polindrom değildir.", Metin, TersMetin);
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
