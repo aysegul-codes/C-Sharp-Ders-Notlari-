7f3946d7f64d68990e84ba00489b10e48b659069
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-05-04 14:46:01 +0300

    Girilen metni her adımda düz ve ters haliyle ekrana yazdırma

---
+++ proje16/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace proje16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan alınan bir metnin her bir karakterini döngü ile işleyerek,
+            //her adımda o ana kadar olan kısmın tersini ve düzünü yan yana yazdıran C# kodunu yazınız."
+            try
+            {
+               
+                Console.Write("Metni Giriniz : ");
+                string Metin = Console.ReadLine();
+
+                string Sonuc = "";
+                string sonuc2 = "";
+
+               
+                for (int i = 0; i < Metin.Length; i++)
+                {
+                    
+                    Sonuc = Sonuc + Metin[i];
+                    sonuc2 = Metin[i] + sonuc2;
+                    Console.WriteLine(sonuc2 + " " + Sonuc);
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+            finally
+            {
+                
+                Console.WriteLine("\nÇıkmak için bir tuşa basın...");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
