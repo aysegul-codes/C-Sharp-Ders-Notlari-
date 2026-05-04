6ce7c337e3f627a6d6a91b71ff939a7de2ab535b
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-05-04 14:02:38 +0300

    feat: kullanıcı metnini harf harf artan şekilde yazdıran uygulama eklendi

---
+++ proje15/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace proje15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       //ÖRN: Kullanıcı tarafından girilen bir metnin ilk harfinden başlayarak her satırda bir fazla harfini yazan
+                //konsol uygulamasının C# dilinde kodunu yazınız.
+
+            try
+          {
+              Console.Write("Metni Giriniz : ");
+              string Metin = Console.ReadLine();
+
+              string Sonuc = "";
+
+              for (int i = 0; i < Metin.Length; i++)
+              {
+                     Sonuc += Metin[i].ToString();
+                     Console.WriteLine(Sonuc);
+              }
+              }
+             catch (Exception ex)
+             {
+                   Console.WriteLine("HATA : {0}", ex.ToString());
+             }
+             finally
+             {
+                  Console.ReadKey();
+             }
+
+            
+
+        }
+    }
+}
