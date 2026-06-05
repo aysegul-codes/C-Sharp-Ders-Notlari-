80f275ae74217a2a9bcb2896365190009eca82e3
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-06-05 14:34:02 +0300

    feat: pervane etkisi gösteren animasyon uygulaması eklendi

---
+++ PROJE21/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace PROJE21
+{
+    using System.Threading;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       //ÖRN : "\", "|", "-" ve "/" sembollerini kullanarak pervane etkisi gösteren (toplamda 100 defa dönen)
+                //bir animasyon yapılmak istenmektedir.
+                //Bunun için konsol uygulamasında gerekli olan C# kodunu yazınız.
+            try
+            {
+                int DelayTime = 100;
+                for (int i = 1; i <= 100; i++)
+                {
+                    Console.Clear();
+                    Console.WriteLine("                    -");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    |");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    /");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    -");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    |");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    -");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    \\");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    |");
+                    Thread.Sleep(DelayTime);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
+
