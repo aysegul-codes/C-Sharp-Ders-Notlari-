db6ebd5d44ef9712418f3f7c1cd3afba83e6e19f
Ayşe Gül Çiftçi <ayseegulcfc@gmail.com>
2026-06-05 14:12:14 +0300

    feat: kullanıcı girdisine göre yıldız desenini yazdıran uygulama eklendi

---
+++ proje19/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace proje19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Kullancı tarafından girilen sayır sayısı kadar her satırda bir fazla * sembolu ekleyerek
+           // ekran yazan konsol uygulamasını C# dilinde kodu yazınız.
+
+            try
+            {
+                Console.Write("Satır Sayısını Giriniz:");
+                int Sayi = Convert.ToInt32(Console.ReadLine());
+                string Sonuc = string.Empty;
+                for (int i = 0; i < Sayi; i++)
+                {
+                    Sonuc += "*";
+                    Console.WriteLine(Sonuc);
+                }
+             }   
+            catch (Exception ex)
+            {
+                    Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+            finally
+            {
+                 Console.ReadKey();
+            }
+        }
+    }
+}
+
+    
+
+   
+
+
+     
